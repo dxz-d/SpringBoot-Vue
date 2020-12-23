33594b6248ad1044e2ae985f75589d40f995359b
dxz-d <1205760997@qq.com>
2020-12-23 15:25:47 +0800

    调整图书馆下面的菜单样式

--- src/components/common/NavMenu.vue
+++ src/components/common/NavMenu.vue
@@ -7,10 +7,10 @@
         text-color="#222"
         active-text-color="red"
         style="min-width: 1300px">
-        <el-menu-item v-for="(item,i) in navList" :key="i" index="item.name">
+        <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
             {{ item.navItem }}
         </el-menu-item>
-        <a href="#nowhere" style="color: #222; float: right; padding: 20px">更多功能</a>
+        <a href="#nowhere" style="color: #222;float: right;padding: 20px;">更多功能</a>
         <i class="el-icon-menu" style="float:right; font-size:45px; color:#222; padding-top:8px"></i>
         <span style="position: absolute; padding-top: 20px; font-size: 20px; font-weight: bold"></span>
     </el-menu>
